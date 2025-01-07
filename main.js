aa89c81db8b4909ad5ef1365269be17e82039949
Nishu09012011 <diobrandowantsmoney@gmail.com>
2025-01-07 18:09:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+ 
+function addUser() {
+   window.location = "HSB.html";
+
+   
+  }
+  function breh(){
+   window.location = "TMH.html";
+   HSB.pause();
+  
+  }
+  function susu(){
+   window.location = "carti.html";
+  }
+  
